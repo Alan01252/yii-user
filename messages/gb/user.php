1edbe4f5f513bf50d0fa2054a5970e0e045ff901
Alan Hollis <me@alanhollis.com>
2012-07-27 13:00:19 +0100

    Add better description for password reset

--- messages/gb/user.php
+++ messages/gb/user.php
@@ -10,4 +10,5 @@ return array(
 	'Retype Password is incorrect.' => 'Passwords do not match',
     'Lost Password?' => 'Lost password?',
 	'Minimal password length 4 symbols.' => 'Minimum password length 4 characters',
+	'Please check your email. An instructions was sent to your email address.' => 'Please check your email. Instructions on how to reset your password have been sent to your email address',
 );
